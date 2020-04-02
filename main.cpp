21a4aebe580dcecd1eac405279025184eb87153b
Riley <lannon.riley@gmail.com>
2020-04-01 19:13:35 -0500

    Revert main, update .gitignore for this branch

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ void file_error(std::string filename) {
 int main (int argc, char *argv[]) {
 	try {
 		// todo: refactor constructors to streamline this process
-		/*std::ifstream infile;
+		std::ifstream infile;
 		infile.open("samples/sample.sin", std::ios::in);
 		Lexer l(infile);
 		Parser *p = new Parser(l);
@@ -39,53 +39,7 @@ int main (int argc, char *argv[]) {
 		// clean-up
 		infile.close();
 		delete p;
-		delete c;*/
-
-		compile_time_evaluator *cte = new compile_time_evaluator();
-		std::shared_ptr<Expression> e = std::make_shared<Literal>(BOOL, "true");
-
-		DataType a_info(
-			BOOL,
-			DataType(),
-			symbol_qualities(
-				true,
-				false,
-				false,
-				false,
-				false,
-				false,
-				false
-			)
-		);
-		Allocation a(
-			a_info,
-			"x",
-			true,
-			e
-		);
-		symbol s("x", "global", 0, a_info, 0);
-		s.set_initialized();
-
-		cte->add_constant(a, s);
-
-		std::shared_ptr<Expression> e2 = std::make_shared<Unary>(
-			std::make_shared<Unary>(
-				std::make_shared<LValue>(
-					"x"
-					),
-				exp_operator::NOT
-				), exp_operator::NOT
-		);
-
-		try {
-			std::cout << "test: " << -(-5) << std::endl;
-			std::string x = cte->evaluate_expression(e2, "global", 0, 1);
-			std::cout << "Evaluated: " << x << std::endl;
-		}
-		catch (std::exception & e) {
-			std::cout << e.what() << std::endl;
-		}
-		delete cte;
+		delete c;
 		
         return 0;
     } catch (std::exception &e) {
